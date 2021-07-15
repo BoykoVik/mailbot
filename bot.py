9b710af14105ba27281010b26b5ed41a29075340
BoykoVik <gexar47@gmail.com>
2021-07-15 23:51:17 +0300

    end

--- bot.py
+++ bot.py
@@ -2,39 +2,90 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
+from settings import SENDERS, TITLES, ONE_MAIL_MATCH, FILES_IMG, TIME_RAGE
+from time import sleep
 
-strFrom = 'gexar47@gmail.com'
-strTo = 'gexar47@mail.ru'
+f = open('list.txt')
+list_emails = []
+quant_mails = 0
+for line in f:
+    if '@' in line:
+        list_emails.append(line)
+        quant_mails += 1
 
-# Create the root message 
+quant_senders = len(SENDERS)
 
-msgRoot = MIMEMultipart('related')
-msgRoot['Subject'] = 'Тут заголовок письма'
-msgRoot['From'] = strFrom
-msgRoot['To'] = strTo
+print('Максимальное число писем = ' + str(quant_senders * ONE_MAIL_MATCH) + ' из ' + str(quant_mails))
+ans = input('продолжаем? да/нет: \n')
+if ans == 'да':
 
-msgRoot.preamble = 'Multi-part message in MIME format.'
+    current_sender = 0 # счетчик отправителей
+    current_title = 0 # счетчик заголовков
+    current_img = 0 # счетчик изображений
 
-msgAlternative = MIMEMultipart('alternative')
-msgRoot.attach(msgAlternative)
+    err_much = 0 # счетчик ошибок
+    good_much = 0 # счетчик отправленных
 
-msgText = MIMEText('Alternative plain text message.')
-msgAlternative.attach(msgText)
+    for mail_to in list_emails:
+        try:
+            strFrom = SENDERS[current_sender][0] #отправитель
+            pwd = SENDERS[current_sender][1]# пароль отправителя
 
-msgText = MIMEText('<b>Тут <i>может</i> быть</b> любой текст.<br><img src="cid:image1"><br>И тут может быть текст', 'html')
-msgAlternative.attach(msgText)
+            # Создание письма
+            msgRoot = MIMEMultipart('related')
+            msgRoot['Subject'] = TITLES[current_title] # задание заголовка
+            msgRoot['From'] = strFrom # задание отправителя
+            msgRoot['To'] = mail_to # задание получателя
 
-#Attach Image 
-fp = open('MMA-&-Box-new.jpg', 'rb') #Read image 
-msgImage = MIMEImage(fp.read())
-fp.close()
+            msgRoot.preamble = 'Multi-part message in MIME format.'
+            msgAlternative = MIMEMultipart('alternative')
+            msgRoot.attach(msgAlternative)
+            msgText = MIMEText('Alternative plain text message.')
+            msgAlternative.attach(msgText)
 
-# Define the image's ID as referenced above
-msgImage.add_header('Content-ID', '<image1>')
-msgRoot.attach(msgImage)
+            msgText = MIMEText('<img src="cid:image1"><br>И тут может быть текст', 'html') #<b>Тут <i>может</i> быть любой текст.</b><br>
+            msgAlternative.attach(msgText)
 
-server = smtplib.SMTP('smtp.gmail.com: 587')
-server.starttls()
-server.login('gexar47@gmail.com', 'nameless47ok')
-server.sendmail(strFrom, strTo, msgRoot.as_string())
-server.quit()
+            #загрузка изображения
+            fp = open(FILES_IMG[current_img], 'rb') #открыл изображение
+            msgImage = MIMEImage(fp.read())#считал
+            fp.close()#закрыл
+
+            # Создание ID соответствия
+            msgImage.add_header('Content-ID', '<image1>')
+            msgRoot.attach(msgImage)# добавление в месседж
+
+            server = smtplib.SMTP('smtp.gmail.com: 587')
+            server.starttls()
+            server.login(strFrom, pwd)
+            server.sendmail(strFrom, mail_to, msgRoot.as_string())
+            server.quit()
+            good_much += 1
+            print(str(good_much) + ' отправлено на адрес ' + mail_to + ' с адреса ' + strFrom)
+        except:
+            err_much += 1
+            print('НЕ отправлено на адрес ' + mail_to)
+            print('НЕ отправлено ВСЕГО ' + str(err_much))
+
+        # Смена отправителя
+        current_sender += 1
+        if current_sender >= len(SENDERS):
+            current_sender = 0
+        # Смена заголовка
+        current_title += 1
+        if current_title >= len(TITLES):
+            current_title = 0
+        # Смена изображения
+        current_img += 1
+        if current_img >= len(FILES_IMG):
+            current_img = 0
+
+        sleep(TIME_RAGE)
+
+
+else:
+    print('отменено')
+
+print('ОТПРАВЛЕНО ' + str(good_much) + ' писем')
+print('ОШИБОК ' + str(err_much) + ' писем')
+print('...................................')
---
+++ settings.py
@@ -0,0 +1,24 @@
+SENDERS = [#тут парами забиваются адреса и пароли
+    ['boxandmma01@gmail.com', 'MoneyisGood7i8'],
+    ['boxandmma02@gmail.com', 'MoneyisGood7i8'],
+    ]
+
+TITLES = [# заголовоки писем
+    'Advertising on boxers and MMA fighters',
+    'Advertising for the whole world on boxers and MMA fighters',
+    'Want advertising to the whole world?',
+    ] 
+
+ONE_MAIL_MATCH = 200 # количество писем с одной почты
+
+FILES_IMG = [
+    'Box-new.jpg',
+    'MMA-&-Box.jpg',
+    'MMA-&-Box-new.jpg',
+    'MMA-&-Box-new-advertising.jpg',
+    'MMA.jpg',
+    'MMA-Box.jpg',
+    'MMA-Box-new.jpg',
+]
+
+TIME_RAGE = 5 # время между отправками
